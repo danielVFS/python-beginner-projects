82919012ee5d3c15e3cbb5d98433781c834c6a21
danielVFS <danielv759153@gmail.com>
2022-08-03 15:35:21 -0300

    feat(GuesNumber): computer guess

--- GuessNumber/main.py
+++ GuessNumber/main.py
@@ -11,8 +11,26 @@ def user_guess(x):
         elif guess_number > random_number:
             print('Sorry, guess again, too high.')
 
-    print("Congrats, you guesses the correct number!")
+    print(f"Congrats, you guesses the number {guess_number} correct!")
 
 
-user_guess(20)
+def computer_guess(x):
+    guess_number = 0
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c' and low != high:
+        guess_number = random.randint(low, high)
+
+        feedback = input(f'Is {guess_number} too high(h), too low(l) or correct(c)? \n').lower()
+        if feedback == 'h':
+            high = guess_number - 1
+        elif feedback == 'l':
+            low = guess_number + 1
+
+    print(f"Congrats, you guesses the number {guess_number} correct!")
+
+
+# user_guess(20)
+computer_guess(1000)
 
