dcf9dadaea0ac79c12d48333b212f267256e774b
danielVFS <danielv759153@gmail.com>
2022-08-03 13:49:14 -0300

    feat(GuessNumber) user guess

--- GuessNumber/main.py
+++ GuessNumber/main.py
@@ -1,1 +1,18 @@
-print('hello world')
+import random
+
+
+def user_guess(x):
+    random_number = random.randint(1, x)
+    guess_number = 0
+    while guess_number != random_number:
+        guess_number = int(input(f'Guess a number between 1 and {x} \n'))
+        if guess_number < random_number:
+            print('Sorry, guess again, too low.')
+        elif guess_number > random_number:
+            print('Sorry, guess again, too high.')
+
+    print("Congrats, you guesses the correct number!")
+
+
+user_guess(20)
+
